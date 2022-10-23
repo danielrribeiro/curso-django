5817fddf73fa9628997b5f6506e9825cc460f31e
@danielrribeiro <danielrodriguesr@gmail.com>
2022-10-23 04:55:39 -0300

    Extraída configuração de banco de dados
    close #38

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from functools import partial
 from pathlib import Path
 
+import dj_database_url
 from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,11 +74,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+default_db_url = f"sqlite:///{str(BASE_DIR / 'db.sqlite3')}"
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 # Password validation
