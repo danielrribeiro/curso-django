a3c1d57462fa58abe1e5dee4be1e43d47b542b9c
@danielrribeiro <danielrodriguesr@gmail.com>
2022-10-27 22:04:02 -0300

    Instalado Sentry
    close #55

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os.path
+
+import os
+import dj_database_url
+import sentry_sdk
+
 from functools import partial
 from pathlib import Path
-
-import dj_database_url
 from decouple import config, Csv
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -171,6 +174,21 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()],
+
+                    # Set traces_sample_rate to 1.0 to capture 100%
+                    # of transactions for performance monitoring.
+                    # We recommend adjusting this value in production.
+                    traces_sample_rate=1.0,
+
+                    # If you wish to associate users to errors (assuming you are using
+                    # django.contrib.auth) you may enable sending PII data.
+                    send_default_pii=True
+                    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
