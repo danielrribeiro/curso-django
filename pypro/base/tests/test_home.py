7798364f927e88bfc613b80ed0b5da9f27eb5cbc
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-03 12:32:34 -0300

    Refatorada url da app base

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -3,10 +3,9 @@ from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
 
-
 @pytest.fixture
 def resp(client):
-    resp = client.get(reverse('home'))
+    resp = client.get(reverse('base:home'))
     return resp
 
 
@@ -19,4 +18,4 @@ def test_title(resp):
 
 
 def test_home_link(resp):
-    assert_contains(resp, f'href="{reverse("home")}">Python Pro</a>')
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
