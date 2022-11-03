418574f0abf8d0ce18dcf0f1b6b7d3b5bf2d4796
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-03 12:23:19 -0300

    Incluída url nomeada com uso de reverse e template tag url

--- pypro/urls.py
+++ pypro/urls.py
@@ -15,17 +15,18 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(
         path('__debug__/', include(debug_toolbar.urls))
     )
