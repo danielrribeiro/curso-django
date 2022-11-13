b03046242d94bc547d2718e6938002985eb75ef0
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-13 03:21:50 -0300

    Criada página de índices de vídeos aperitivos
    close #97

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
+    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'youtube_id': 'ScmQ4I5Qr5s'},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
-        'instalacao-windows': {'titulo': '', 'youtube_id': 'ScmQ4I5Qr5s'},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
