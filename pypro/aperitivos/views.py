2fba81ce808e9fcd9c92172f63e37baa36156573
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-13 04:12:57 -0300

    Acesso a atributos através de contexto de template
    hold #99

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
-videos = [
-    {'slug': 'motivacao', 'titulo': 'Vídeo Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'youtube_id': 'ScmQ4I5Qr5s'},
-]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+class Video:
+    def __init__(self, slug, titulo, youtube_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.youtube_id = youtube_id
+
+
+videos = [
+    Video('motivacao', 'Vídeo Aperitivo: Motivação', '2aYplgJrPDU'),
+    Video('instalacao-windows', 'Instalação Windows', 'ScmQ4I5Qr5s'),
+]
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
