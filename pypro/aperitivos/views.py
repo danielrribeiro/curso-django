150b9077660f78041f2a92e2c66464f012f18d6d
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-15 02:05:28 -0300

    Alterada classe para Modelo do Django
    Parcial de #109

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, youtube_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.youtube_id = youtube_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Vídeo Aperitivo: Motivação', '2aYplgJrPDU'),
-    Video('instalacao-windows', 'Instalação Windows', 'ScmQ4I5Qr5s'),
+    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', youtube_id='2aYplgJrPDU'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', youtube_id='ScmQ4I5Qr5s'),
 ]
 videos_dct = {v.slug: v for v in videos}
 
