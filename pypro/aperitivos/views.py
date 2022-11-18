87db914aca80eeb2572be15b299ef3fe4cecc783
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-18 19:08:25 -0300

    Implementado índice com acesso a banco de dados
    Parcial de #109

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', youtube_id='2aYplgJrPDU'),
-    Video(slug='instalacao-windows', titulo='Instalação Windows', youtube_id='ScmQ4I5Qr5s'),
-]
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
