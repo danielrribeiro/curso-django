19cad01bd83dfa58835122349836d2360226e85e
@danielrribeiro <danielrodriguesr@gmail.com>
2022-11-24 00:47:07 -0300

    Implementada funcionalidade de mostrar módulos em Dropdown
    Parcial de #115

--- pypro/settings.py
+++ pypro/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pypro.base',
     'pypro.aperitivos',
+    'pypro.modulos',
+    'ordered_model',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pypro.modulos.context_processors.listar_modulos',
             ],
         },
     },
