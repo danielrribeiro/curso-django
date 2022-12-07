833ca2e169a7ad44b053eb297b08f80d0e7fda9c
@danielrribeiro <danielrodriguesr@gmail.com>
2022-12-07 13:05:36 -0300

    Criado Índice para Módulos
    hold #140

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from pypro.modulos.views import detalhe, aula
+from pypro.modulos.views import detalhe, aula, indice
 
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', detalhe, name='detalhe'),
     path('aulas/<slug:slug>', aula, name='aula'),
+    path('', indice, name='indice'),
 ]
